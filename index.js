e8e50bc0adc5aad8453c10ac1bb2dd98b9c964be
+ <+>
2022-11-29 14:25:30 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -7,11 +7,11 @@ console.log(multiply);
 var random = Math.floor(Math.random() * 11);
 console.log(random);
 
-var num3 = 22
+var num3 = 34
 var num4 = 6
 
 var mod=num3 % num4
 console.log(mod);
 
-var max = Math.max(1, 4, 8, 20);
+var max = Math.max(5, 9, 11, 20);
 console.log(max)
