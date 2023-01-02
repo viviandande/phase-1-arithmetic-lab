b9a0c29ed8525927c9f55361241863c119d921f7
+ <+>
2023-01-02 13:46:50 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 
-let num1=31
-let num2=2
-const multiply = Math.imul(num1, num2);
+let num1 = 2
+let num2 = 31
+
+const multiply = Math.imul(num1,num2);
 console.log(multiply);
 
-var random = Math.floor(Math.random() * 11);
+const random = Math.floor(Math.random () * 10);
 console.log(random);
 
-var num3 = 34
-var num4 = 6
+let num3 = 28
+let num4 = 6
 
-var mod=num3 % num4
+const mod = (num3 % num4);
 console.log(mod);
 
-var max = Math.max(5, 9, 11, 20);
-console.log(max)
+const max = Math.max(1,4,7,20);
+console.log(max);
